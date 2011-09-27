1640eefaa8dbcfe7cbc715f29e1952be22635055
tryforos <tryforos@gmail.com>
2011-09-27 16:15:28 -0400

    finished ch 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 SampleApp::Application.routes.draw do
-  #get "pages/home"
+  get "users/new"
 
-  root :to => "pages#home"
+  match '/signup',  :to => 'users#new'
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+
+  root :to => "pages#home"
+  #get "pages/home"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
